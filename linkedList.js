644ad8d55c01f659e43797e70bca52c9ce378967
TipDevs <tipdevs@gmail.com>
2025-08-24 16:59:57 +0100

    Add LinkedList class and its methods skeleton

---
+++ linkedList.js
@@ -0,0 +1,36 @@
+class LinkedList {
+  constructor() {
+    this.list = null;
+  }
+  append(value) {
+    // adds new node containing value to the end of the list
+  }
+  prepend(value) {
+    // adds new node containing value to the start of the list
+  }
+  size() {
+    // returns the number of nodes in the list
+  }
+  head() {
+    // returns the first node in the list
+  }
+  tail() {
+    // returns the last node in the list
+  }
+  at(index) {
+    // returns the node at the given index
+  }
+  pop() {
+    // removes the last element from the list
+  }
+  contains(value) {
+    // returns true if the passed in value is in the list and otherwise return false
+  }
+  find(value) {
+    // returns the index of the node containing value, or null if not found
+  }
+  toString() {
+    // represents the LinkedList objects as strings
+  }
+}
+
